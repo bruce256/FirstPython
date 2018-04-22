591bc115758e7a4edd4652de5182a71d62be3271
lvsheng <lvsheng.ls@alibaba-inc.com>
2018-04-22 23:34:41 +0800

    file

---
+++ FileTest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# 写文件
+with open("test.txt", "wt", encoding="utf-8") as out_file:
+	out_file.write("该文本会写入到文件中\n看到我了吧！")
+
+# Read a file
+with open("test.txt", "rt", encoding="utf-8") as in_file:
+	text = in_file.read()
+
+print(text)

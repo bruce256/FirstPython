2690e113c47cea98a6b18d9356b7e7162491e399
lvsheng <lvsheng.ls@alibaba-inc.com>
2018-05-26 23:48:53 +0800

    scatter

---
+++ scatterImage.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_excel("/study/python/income.xlsx")
+print(df.head(10))
+fig = df.plot(kind="scatter", x="height", y="income").get_figure()
+fig.savefig("/study/scatter.jpg")
+
+url = 'http://s3.amazonaws.com/assets.datacamp.com/course/dasi/present.txt'
+present = pd.read_table("/Users/lvsheng/Movies/present.txt", sep=' ')
+print(present.shape)
+
+print(present.columns)
+print(present.head(63))
+present_year = present.set_index('year')
+plt = present_year.plot().get_figure()
+plt.savefig("/study/present.jpg")
+plt = present_year.plot(kind="bar").get_figure()
+plt.savefig("/study/present_bar.jpg")
+plt = present_year.plot(kind="bar", stacked=True).get_figure()
+plt.savefig("/study/present_bar_stacked.jpg")

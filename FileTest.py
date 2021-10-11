82c3bfb0f8564fc83522d75a3dff9529f36f86ff
lvsheng <lvsheng@sensorsdata.cn>
2021-10-11 22:37:01 +0800

    写文件

--- FileTest.py
+++ FileTest.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 # 写文件
-with open("test.txt", "wt", encoding="utf-8") as out_file:
-	out_file.write("该文本会写入到文件中\n看到我了吧！")
+with open("redis.txt", "wt", encoding="utf-8") as out_file:
+    for i in range(1000000):
+        out_file.write("set key" + str(i) + " value" + str(i) + "\n")
 
 # Read a file
-with open("test.txt", "rt", encoding="utf-8") as in_file:
-	text = in_file.read()
+# with open("test.txt", "rt", encoding="utf-8") as in_file:
+# 	text = in_file.read()
 
-print(text)
+# print(text)
